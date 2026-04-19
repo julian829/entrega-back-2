ea35d46e85080e2a08a66612a07d25a40a033f2e
Juliangiraldo <juliangiraldobravo@gmail.com>
2026-04-19 18:13:20 -0500

    organizacion de dependencias en el pom

--- src/main/java/com/llanera/carnellanera/mapper/ProductoMapper.java
+++ src/main/java/com/llanera/carnellanera/mapper/ProductoMapper.java
@@ -1,5 +1,13 @@
 package com.llanera.carnellanera.mapper;
 
-public class ProductoMapper {
-    
+import com.llanera.carnellanera.dto.ProductoDTO;
+import com.llanera.carnellanera.model.Producto;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface ProductoMapper {
+
+    ProductoDTO toDto(Producto producto);
+
+    Producto toEntity(ProductoDTO dto);
 }
