b41567b1c8dcf44685388f0395a03e017bf92604
Juliangiraldo <juliangiraldobravo@gmail.com>
2026-04-19 18:27:08 -0500

    agregue lo nuevo

--- src/main/java/com/llanera/carnellanera/service/ProductoService.java
+++ src/main/java/com/llanera/carnellanera/service/ProductoService.java
@@ -1,5 +1,28 @@
 package com.llanera.carnellanera.service;
 
+import com.llanera.carnellanera.dto.ProductoDTO;
+import com.llanera.carnellanera.model.Producto;
+import com.llanera.carnellanera.mapper.ProductoMapper;
+import com.llanera.carnellanera.repository.ProductoRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductoService {
-    
+
+    private final ProductoRepository repository;
+    private final ProductoMapper mapper;
+
+    public ProductoService(ProductoRepository repository, ProductoMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
+    public ProductoDTO guardar(ProductoDTO dto) {
+
+        Producto producto = mapper.toEntity(dto);
+
+        Producto guardado = repository.save(producto);
+
+        return mapper.toDto(guardado);
+    }
 }
